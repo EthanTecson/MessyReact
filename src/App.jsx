f9b979a8f8b19d18052d50af179288123938808c
Ethan Tecson <ethantecson@gmail.com>
2024-10-23 22:41:52 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Student from "./Student.jsx";
+
+function App() {
+  return (
+    <>
+      <Student name="Spongebob" age="30" isStudent={true} />
+      <Student />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,17 @@
+function Card() {
+  return (
+    <div className="card">
+      <img
+        className="card-image"
+        src="https://via.placeholder.com/150"
+        alt="profile picture"
+      ></img>
+      <h2 className="card-title">Ethan Tecson</h2>
+      <p className="card-text">
+        Software Engineer at JP Morgan (Hopefully) and I like computers
+      </p>
+    </div>
+  );
+}
+
+export default Card;
