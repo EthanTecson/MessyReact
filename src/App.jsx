3212d2f4c838f018de116c1fe82990f9a3604cf1
Ethan Tecson <ethantecson@gmail.com>
2024-10-29 10:54:11 -0400

    last few notes on hooks

--- src/App.jsx
+++ src/App.jsx
@@ -17,24 +17,49 @@
  *                   AND a setter function to update its value in the Virtual DOM.
  *                    [name, setName]
  *      onChange() - Triggers a function every time the value of the input changesTriggers a function every time the value of the input changesTriggers a function every time the value of the input changesTriggers a function every time the value of the input changesTriggers a function every time the value of the input changesTriggers a function every time the value of the input changesTriggers a function every time the value of the input changesTriggers a function every time the value of the input changes 
+ *      onClick() - Function can be triggered based on being clicked
  *      
  *      useEffect() - React Hook that tells React DO SOME CODE WHEN (pick one):
  *                      This component re-renders
  *                      This component mounts
  *                      The state of a value
 
- *      useEffect(function, [dependencies])
+ *        useEffect(function, [dependencies])
 
-        1. useEffect(() => {})    // Runs after every re-render
-        2. useEffect(() => {}, [])  // Runs only on mount
-        3. useEffect(() => {}, [value]) // Runs on mount + when value changes
+          1. useEffect(() => {})    // Runs after every re-render
+          2. useEffect(() => {}, [])  // Runs only on mount
+          3. useEffect(() => {}, [value]) // Runs on mount + when value changes
 
-        // USES
-        // #1 Event Listeners
-        // #2 DOM Manipulation
-        // #3 Subscriptions (real-time updates)
-        // #4 Fetching Data from an API
-        // #5 Clean up when a component unmounts
+          // USES
+          // #1 Event Listeners
+          // #2 DOM Manipulation
+          // #3 Subscriptions (real-time updates)
+          // #4 Fetching Data from an API
+          // #5 Clean up when a component unmounts
+
+      useContext() = React hook that allows you to share values between multiple levels of
+                    components without passing props through each level
+
+        PROVIDER COMPONENT
+          1. import {createContext} from 'react';
+          2. export const MyContext = createContext();
+          3. <MyContext.Provider value={value}>
+                <Child />
+            </MyContext.Provider>
+        
+        CONSUMER COMPONENTS
+          1. import React, {useContext} from 'react';
+              import { MyContext } from './ComponentA';
+          2. const value = useContext(MyContext);
+
+      useRef() = "use Referene" Does not cause re-renders when its value changes.
+                  When you want a component to "remember" some information, but you 
+                  don't want that information to trigger new renders.
+
+          1. Accessing/Interacting with DOM elements
+          2. Handling Focus, Animations, Transitions
+          3. Managing Timers and Intervals
+
  */
 
 import List from "./List.jsx";
